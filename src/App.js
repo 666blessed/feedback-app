7f2f98148fc14fb80e1f51c7963b1c64d32d95bd
Katkov Kirill <katkov.kirill10@gmail.com>
2022-01-08 15:27:23 +0200

    adding feedback implemented

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import {v4 as uuidv4} from 'uuid';
 
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = id => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
+    }
+
+    const addFeedback = newFeedback => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback])
     }
 
 
@@ -21,7 +27,7 @@ function App() {
         <>
         <Header />
         <div className='container'>
-            <FeedbackForm/>
+            <FeedbackForm handleAdd={addFeedback}/>
             <FeedbackStats feedback={feedback}></FeedbackStats>
             <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>        
         </div>
