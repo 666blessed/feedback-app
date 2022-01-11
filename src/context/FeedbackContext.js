c647ddc6c2bdc6ef8535484557e4abcbf5bab527
Kirill Katkov <78655088+666blessed@users.noreply.github.com>
2022-01-12 00:49:58 +0200

    Update FeedbackContext.js

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -62,7 +62,8 @@ export const FeedbackProvider = ({children}) => {
 
         const data = await response.json();
 
-        setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item))
+        setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item));
+        setFeedbackEdit({item: {}, edit: false});
     };
 
     return <FeedbackContext.Provider value={{
